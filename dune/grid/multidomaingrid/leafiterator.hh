2538593cb43329ca389163dee64dbf1207f891fd
Steffen Müthing <muethisn@jig.informatik.uni-stuttgart.de>
2009-07-10 15:39:02 +0200

    LeafIterator implementation

---
+++ dune/grid/multidomaingrid/leafiterator.hh
@@ -0,0 +1,32 @@
+#ifndef DUNE_MULTIDOMAINGRID_LEAFITERATOR_HH
+#define DUNE_MULTIDOMAINGRID_LEAFITERATOR_HH
+
+namespace Dune {
+
+namespace mdgrid {
+
+template<int codim, PartitionIteratorType pitype, typename GridImp>
+class LeafIteratorWrapper :
+    public EntityPointerWrapper<codim,GridImp>
+{
+
+  typedef typename GridImp::HostGridType::Traits::template Codim<codim>::template Partition<pitype>::LeafIterator HostLeafIterator;
+
+  explicit LeafIteratorWrapper(const HostLeafIterator& hostIterator) :
+    _hostIterator(hostIterator)
+  {}
+
+  void increment() {
+    ++_hostIterator;
+    _hostEntity.reset(_hostIterator);
+  }
+
+  HostLeafIterator _hostIterator;
+
+};
+
+} // namespace mdgrid
+
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAINGRID_LEAFITERATOR_HH
