66c23da1f94bb08cafd7582e3d1a2bf839c0f891
Steffen Müthing <muethisn@jig.informatik.uni-stuttgart.de>
2009-07-10 15:38:42 +0200

    EntityPointer implementation

---
+++ dune/grid/multidomaingrid/entitypointer.hh
@@ -0,0 +1,59 @@
+#ifndef DUNE_MULTIDOMAINGRID_ENTITYPOINTER_HH
+#define DUNE_MULTIDOMAINGRID_ENTITYPOINTER_HH
+
+namespace Dune {
+
+namespace mdgrid {
+
+template<int codim, typename GridImp>
+class EntityPointerWrapper
+{
+
+  static const int dim = GridImp::dimension;
+
+public:
+
+  typedef EntityPointerWrapper EntityPointerImpl;
+
+  static const int codimension = codim;
+
+  typedef typename GridImp::Traits::template Codim<codim>::Entity Entity;
+  typedef EntityPointerWrapper<codim,GridImp> Base;
+
+  typedef typename GridImp::HostGridType::Traits::template Codim<codim>::EntityPointer HostEntityPointer;
+
+  EntityPointerWrapper(const HostEntityPointer& hostEntityPointer) :
+    _entityWrapper(hostEntityPointer)
+  {}
+
+  EntityPointerWrapper(const EntityWrapper<codim,dim,GridImp>& entity) :
+    _entityWrapper(entity._hostEntityPointer)
+  {}
+
+  bool equals(const EntityPointerWrapper<codim,GridImp>& rhs) const {
+    return _entityWrapper == rhs._entityWrapper;
+  }
+
+  Entity& dereference() const {
+    return _entityWrapper;
+  }
+
+  void compactify() {
+    _entityWrapper.compactify();
+  }
+
+  int level() const {
+    return _entityWrapper.level();
+  }
+
+protected:
+
+  MakeableEntityWrapper<codim,dim,GridImp> _entityWrapper;
+
+};
+
+} // namespace mdgrid
+
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAINGRID_ENTITYPOINTER_HH
