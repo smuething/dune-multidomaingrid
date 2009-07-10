6bda50185df2d47415eabbb17bcc1378278259f5
Steffen Müthing <muethisn@jig.informatik.uni-stuttgart.de>
2009-07-10 15:38:53 +0200

    HierarchicIterator implementation

---
+++ dune/grid/multidomaingrid/hierarchiciterator.hh
@@ -0,0 +1,32 @@
+#ifndef DUNE_MULTIDOMAINGRID_HIERARCHICITERATOR_HH
+#define DUNE_MULTIDOMAINGRID_HIERARCHICITERATOR_HH
+
+namespace Dune {
+
+namespace mdgrid {
+
+template<typename GridImp>
+class HierarchicIteratorWrapper :
+    public EntityPointerWrapper<0,GridImp>
+{
+
+  typedef typename GridImp::HostGridType::Traits::template Codim<0>::Entity::HierarchicIterator HostHierarchicIterator;
+
+  explicit HierarchicIteratorWrapper(const HostHierarchicIterator& hostIterator) :
+    _hostIterator(hostIterator)
+  {}
+
+  void increment() {
+    ++_hostIterator;
+    _hostEntity.reset(_hostIterator);
+  }
+
+  HostHierarchicIterator _hostIterator;
+
+};
+
+} // namespace mdgrid
+
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAINGRID_HIERARCHICITERATOR_HH
