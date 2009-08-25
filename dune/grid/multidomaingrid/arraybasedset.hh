a0b336135834dc6be38a68ca2d1126dea4e8fb0f
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2009-08-25 16:15:55 +0200

    added 2 new SubDomainSet implementations
    ArrayBasedSet: can contain up to a predefined number
    of subdomains with arbitrary values and stores them
    in a sorted array
    
    SingleValueSet: can only contain up to one arbitrary
    subdomain.

---
+++ dune/grid/multidomaingrid/arraybasedset.hh
@@ -0,0 +1,159 @@
+#ifndef DUNE_MULTIDOMAINGRID_ARRAYBASEDSET_HH
+#define DUNE_MULTIDOMAINGRID_ARRAYBASEDSET_HH
+
+#include <limits>
+#include <cstddef>
+#include <cstdint>
+#include <dune/common/static_assert.hh>
+#include <dune/common/typetraits.hh>
+#include <dune/common/exceptions.hh>
+#include <boost/integer_traits.hpp>
+#include <type_traits>
+#include <cassert>
+#include <strings.h>
+
+namespace Dune {
+
+namespace mdgrid {
+
+template<typename SubDomainType, std::size_t capacity>
+class ArrayBasedSet;
+
+
+template<typename SubDomainType, std::size_t capacity>
+bool setContains(const ArrayBasedSet<SubDomainType,capacity>& a,
+                 const ArrayBasedSet<SubDomainType,capacity>& b);
+
+
+template<typename SubDomainType, std::size_t capacity>
+void setAdd(ArrayBasedSet<SubDomainType,capacity>& a,
+            const ArrayBasedSet<SubDomainType,capacity>& b);
+
+
+template<typename SubDomainType, std::size_t capacity>
+class ArrayBasedSet {
+
+  friend bool setContains<>(const ArrayBasedSet<SubDomainType,capacity>& a,
+                        const ArrayBasedSet<SubDomainType,capacity>& b);
+
+  friend void setAdd<>(ArrayBasedSet<SubDomainType,capacity>& a,
+                   const ArrayBasedSet<SubDomainType,capacity>& b);
+
+public:
+  static const std::size_t maxSize = capacity;
+  static const SubDomainType emptyTag = boost::integer_traits<SubDomainType>::const_max;
+  typedef SubDomainType DomainType;
+  typedef typename std::array<DomainType,maxSize>::iterator ArrayIterator;
+  typedef typename std::array<DomainType,maxSize>::const_iterator Iterator;
+  typedef ArrayBasedSet<SubDomainType,capacity> This;
+
+  enum SetState {emptySet,simpleSet,multipleSet};
+
+  Iterator begin() const {
+    return _set.begin();
+  }
+
+  Iterator end() const {
+    return _set.begin() + _size;
+  }
+
+  bool contains(DomainType domain) const {
+    return std::binary_search(_set.begin(),_set.begin() + _size,domain);
+  }
+
+  template<typename Set>
+  bool containsSet(const Set& set) const {
+    return setContains(*this,set);
+  }
+
+  bool simple() const {
+    return _size == 1;
+  }
+
+  bool empty() const {
+    return _size == 0;
+  }
+
+  SetState state() const {
+    switch (_size) {
+    case 0:
+      return emptySet;
+    case 1:
+      return simpleSet;
+    default:
+      return multipleSet;
+    }
+  }
+
+  std::size_t size() const {
+    return _size;
+  }
+
+  void clear() {
+    _size = 0;
+  }
+
+  void add(DomainType domain) {
+    assert(_size < maxSize);
+    assert(!std::binary_search(_set.begin(),_set.begin()+_size,domain));
+    _set[_size++] = domain;
+    std::sort(_set.begin(),_set.begin()+_size);
+  }
+
+  void remove(DomainType domain) {
+    ArrayIterator it = std::lower_bound(_set.begin(),_set.begin()+_size,domain);
+    assert(*it == domain);
+    *it = emptyTag;
+    std::sort(_set.begin(),_set.end() + (_size--));
+  }
+
+  void set(DomainType domain) {
+    _size = 1;
+    _set[0] = domain;
+  }
+
+  template<typename Set>
+  void addSet(const Set& set) {
+    setAdd(*this,set);
+  }
+
+  int domainOffset(DomainType domain) const {
+    Iterator it = std::lower_bound(_set.begin(),_set.begin()+_size,domain);
+    assert(*it == domain);
+    return it - _set.begin();
+  }
+
+  ArrayBasedSet() :
+    _size(0)
+  {}
+
+private:
+  std::size_t _size;
+  std::array<DomainType,maxSize> _set;
+
+};
+
+
+template<typename SubDomainType, std::size_t capacity>
+inline bool setContains(const ArrayBasedSet<SubDomainType,capacity>& a,
+                        const ArrayBasedSet<SubDomainType,capacity>& b) {
+  return std::includes(a._set.begin(),a._set.begin() + a._size,b._set.begin(),b._set.begin() + b._size);
+}
+
+template<typename SubDomainType, std::size_t capacity>
+inline bool setAdd(ArrayBasedSet<SubDomainType,capacity>& a,
+                   const ArrayBasedSet<SubDomainType,capacity>& b) {
+  std::array<SubDomainType,2*capacity> tmp;
+  typename std::array<SubDomainType,2*capacity>::iterator it = std::set_union(a._set.begin(), a._set.begin() + a._size,
+                                                                              b._set.begin(), b._set.begin() + b._size,
+                                                                              tmp.begin());
+  a._size = it - tmp.begin();
+  assert(a._size <= capacity);
+  std::copy(tmp.begin(),++it,a._set.begin());
+}
+
+} // namespace mdgrid
+
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAINGRID_ARRAYBASEDSET_HH
---
+++ dune/grid/multidomaingrid/singlevalueset.hh
@@ -0,0 +1,139 @@
+#ifndef DUNE_MULTIDOMAINGRID_SINGLEVALUESET_HH
+#define DUNE_MULTIDOMAINGRID_SINGLEVALUESET_HH
+
+#include <limits>
+#include <cstddef>
+#include <cstdint>
+#include <dune/common/static_assert.hh>
+#include <dune/common/typetraits.hh>
+#include <dune/common/exceptions.hh>
+#include <boost/integer_traits.hpp>
+#include <type_traits>
+#include <cassert>
+#include <strings.h>
+
+namespace Dune {
+
+namespace mdgrid {
+
+template<typename SubDomainType>
+class SingleValueSet;
+
+
+template<typename SubDomainType>
+bool setContains(const SingleValueSet<SubDomainType>& a,
+                 const SingleValueSet<SubDomainType>& b);
+
+
+template<typename SubDomainType>
+void setAdd(SingleValueSet<SubDomainType>& a,
+            const SingleValueSet<SubDomainType>& b);
+
+
+template<typename SubDomainType>
+class SingleValueSet {
+
+  friend bool setContains<>(const SingleValueSet<SubDomainType>& a,
+                        const SingleValueSet<SubDomainType>& b);
+
+  friend void setAdd<>(SingleValueSet<SubDomainType>& a,
+                   const SingleValueSet<SubDomainType>& b);
+
+public:
+  static const std::size_t maxSize = 1;
+  static const SubDomainType emptyTag = boost::integer_traits<SubDomainType>::const_max;
+  typedef SubDomainType DomainType;
+  typedef const SubDomainType* Iterator;
+  typedef SingleValueSet<SubDomainType,1> This;
+
+  enum SetState {emptySet,simpleSet,multipleSet};
+
+  Iterator begin() const {
+    return &_set;
+  }
+
+  Iterator end() const {
+    return &_set + (_set != emptyTag ? 1 : 0);
+  }
+
+  bool contains(DomainType domain) const {
+    return _set == domain;
+  }
+
+  template<typename Set>
+  bool containsSet(const Set& set) const {
+    return setContains(*this,set);
+  }
+
+  bool simple() const {
+    return _set != emptyTag;
+  }
+
+  bool empty() const {
+    return _set == emptyTag;
+  }
+
+  SetState state() const {
+    return _set == emptyTag ? emptySet : simpleSet;
+  }
+
+  std::size_t size() const {
+    return _set == emptyTag ? 0 : 1;
+  }
+
+  void clear() {
+    _set = emptyTag;
+  }
+
+  void add(DomainType domain) {
+    assert(_set == emptyTag);
+    _set = domain;
+  }
+
+  void remove(DomainType domain) {
+    assert(_set == domain);
+    _set = emptyTag;
+  }
+
+  void set(DomainType domain) {
+    _set = domain;
+  }
+
+  template<typename Set>
+  void addSet(const Set& set) {
+    setAdd(*this,set);
+  }
+
+  int domainOffset(DomainType domain) const {
+    assert(_set != emptyTag);
+    return 0;
+  }
+
+  SingleValueSet() :
+    _set(emptyTag)
+  {}
+
+private:
+  SubDomainType _set;
+
+};
+
+
+template<typename SubDomainType>
+inline bool setContains(const SingleValueSet<SubDomainType>& a,
+                        const SingleValueSet<SubDomainType>& b) {
+  return a._set == b._set;
+}
+
+template<typename SubDomainType>
+inline bool setAdd(SingleValueSet<SubDomainType>& a,
+                   const SingleValueSet<SubDomainType>& b) {
+  assert(a._set == SingleValueSet<SubDomainType>::emptyTag);
+  a._set = b._set;
+}
+
+} // namespace mdgrid
+
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAINGRID_SINGLEVALUESET_HH
