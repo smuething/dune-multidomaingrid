2e4cc944c80b8a886bcb1f2d1bbb8801350c6b79
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2010-12-01 08:35:05 +0100

    new controller for existing pairwise interface iteration

--- dune/grid/multidomaingrid/subdomaininterfaceiterator.hh
+++ dune/grid/multidomaingrid/subdomaininterfaceiterator.hh
@@ -7,6 +7,9 @@ namespace Dune {
 
 namespace mdgrid {
 
+template<typename SubDomainIndexType>
+class SubDomainToSubDomainController;
+
 template<typename GridImp,
          typename WrapperImp,
          typename GridView,
@@ -47,6 +50,9 @@ class SubDomainInterfaceIterator : public ForwardIteratorFacade<SubDomainInterfa
 
   template<typename, typename, typename, typename>
   friend class ForwardIteratorFacade;
+
+  template<typename>
+  friend class SubDomainToSubDomainController;
 
 protected:
 
@@ -254,13 +260,92 @@ private:
 
 };
 
+
+template<typename SubDomainIndexType>
+class SubDomainToSubDomainController
+{
+
+  template<typename GridImp,
+           typename WrapperImp,
+           typename GridView,
+           typename HostGridView,
+           typename IntersectionType,
+           typename IntersectionController
+           >
+  friend class SubDomainInterfaceIterator;
+
+  template<typename Iterator>
+  bool incrementToNextValidEntity(Iterator& it) {
+    while (it._hostIterator != it._hostEnd) {
+      if (it._gridView.indexSet().containsForSubDomain(_subDomain1,*it._hostIterator)) {
+        it._hostIntersectionIterator = it._hostGridView.ibegin(*it._hostIterator);
+        it._hostIntersectionEnd = it._hostGridView.iend(*it._hostIterator);
+        return true;
+      }
+      ++it._hostIterator;
+    }
+    return false;
+  }
+
+  template<typename Iterator>
+  void incrementToNextValidPosition(Iterator& it) {
+    for (;;) {
+      while(it._hostIntersectionIterator != it._hostIntersectionEnd) {
+        if (it._hostIntersectionIterator->neighbor() && it._gridView.indexSet().containsForSubDomain(_subDomain2,*it._hostIntersectionIterator->outside())) {
+          return;
+        }
+        ++it._hostIntersectionIterator;
+      }
+      ++it._hostIterator;
+      if (!incrementToNextValidEntity(it)) {
+        return;
+      }
+    }
+  }
+
+  template<typename Iterator>
+  void increment(Iterator& it) {
+    ++it._hostIntersectionIterator;
+    incrementToNextValidPosition(it);
+  }
+
+  template<typename Iterator>
+  void incrementToStartPosition(Iterator& it)
+  {
+    if (incrementToNextValidEntity(it)) {
+      incrementToNextValidPosition(it);
+    }
+  }
+
+  SubDomainToSubDomainController(SubDomainIndexType subDomain1, SubDomainIndexType subDomain2)
+    : _subDomain1(subDomain1)
+    , _subDomain2(subDomain2)
+  {}
+
+  SubDomainIndexType subDomain1() const
+  {
+    return _subDomain1;
+  }
+
+  SubDomainIndexType subDomain2() const
+  {
+    return _subDomain2;
+  }
+
+  const SubDomainIndexType _subDomain1;
+  const SubDomainIndexType _subDomain2;
+};
+
+
 template<typename GridImp>
 class LeafSubDomainInterfaceIterator :
     public SubDomainInterfaceIterator<GridImp,
                                       LeafSubDomainInterfaceIterator<GridImp>,
                                       typename GridImp::LeafGridView,
                                       typename detail::HostGridAccessor<GridImp>::Type::LeafGridView,
-                                      LeafSubDomainInterfaceIterator<GridImp> >
+                                      LeafSubDomainInterfaceIterator<GridImp>,
+                                      SubDomainToSubDomainController<typename GridImp::SubDomainIndexType>
+                                      >
 {
 
   template<typename, typename, typename, typename, typename>
@@ -272,18 +357,22 @@ class LeafSubDomainInterfaceIterator :
   template<typename,typename>
   friend class MultiDomainGrid;
 
+  typedef SubDomainToSubDomainController<typename GridImp::SubDomainIndexType> Controller;
+
   typedef SubDomainInterfaceIterator<GridImp,
                                      LeafSubDomainInterfaceIterator<GridImp>,
                                      typename GridImp::LeafGridView,
                                      typename GridImp::HostGridType::LeafGridView,
-                                     LeafSubDomainInterfaceIterator<GridImp> > Base;
+                                     LeafSubDomainInterfaceIterator<GridImp>,
+                                     Controller
+                                     > Base;
 
   typedef LeafSubDomainInterfaceIterator<GridImp> Intersection;
   typedef typename GridImp::SubDomainIndexType SubDomainIndexType;
   typedef SubDomainIndexType SubDomainType DUNE_DEPRECATED;
 
   LeafSubDomainInterfaceIterator(const GridImp& grid, SubDomainIndexType subDomain1, SubDomainIndexType subDomain2, bool end=false) :
-    Base(grid.leafView(),grid._hostGrid.leafView(),subDomain1,subDomain2,end)
+    Base(grid.leafView(),grid._hostGrid.leafView(),Controller(subDomain1,subDomain2),end)
   {}
 
 };
@@ -295,7 +384,9 @@ class LevelSubDomainInterfaceIterator :
                                       LevelSubDomainInterfaceIterator<GridImp>,
                                       typename GridImp::LevelGridView,
                                       typename detail::HostGridAccessor<GridImp>::Type::LevelGridView,
-                                      LevelSubDomainInterfaceIterator<GridImp> >
+                                      LevelSubDomainInterfaceIterator<GridImp>,
+                                      SubDomainToSubDomainController<typename GridImp::SubDomainIndexType>
+                                      >
 {
 
   template<typename, typename, typename, typename, typename>
@@ -307,18 +398,22 @@ class LevelSubDomainInterfaceIterator :
   template<typename,typename>
   friend class MultiDomainGrid;
 
+  typedef SubDomainToSubDomainController<typename GridImp::SubDomainIndexType> Controller;
+
   typedef SubDomainInterfaceIterator<GridImp,
                                      LevelSubDomainInterfaceIterator<GridImp>,
                                      typename GridImp::LevelGridView,
                                      typename GridImp::HostGridType::LevelGridView,
-                                     LevelSubDomainInterfaceIterator<GridImp> > Base;
+                                     LevelSubDomainInterfaceIterator<GridImp>,
+                                     Controller
+                                     > Base;
 
   typedef LevelSubDomainInterfaceIterator<GridImp> Intersection;
   typedef typename GridImp::SubDomainIndexType SubDomainIndexType;
   typedef SubDomainIndexType SubDomainType DUNE_DEPRECATED;
 
   LevelSubDomainInterfaceIterator(const GridImp& grid, SubDomainIndexType subDomain1, SubDomainIndexType subDomain2, int level, bool end=false) :
-    Base(grid.levelView(level),grid._hostGrid.levelView(level),subDomain1,subDomain2,end)
+    Base(grid.levelView(level),grid._hostGrid.levelView(level),Controller(subDomain1,subDomain2),end)
   {}
 
 };
