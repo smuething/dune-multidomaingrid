84c345f777792beb70e21695828a760f80d1c6a9
Steffen Müthing <muething@dune-project.org>
2015-05-08 17:57:35 +0200

    [Tests] Clean up testintersectionconversion
    - Use new entity-based grid API
    
    - Use range-based for loops
    
    - Better exception handling
    
    - Silence warnings

--- test/testintersectionconversion.cc
+++ test/testintersectionconversion.cc
@@ -2,6 +2,7 @@
 #include <config.h>
 #endif
 
+#include <dune/common/unused.hh>
 #include <dune/grid/yaspgrid.hh>
 #include <dune/grid/multidomaingrid.hh>
 #include <iostream>
@@ -10,39 +11,54 @@
 int main(int argc, char** argv)
 {
 
-  Dune::FieldVector<double,2> L(1.0);
-  Dune::array<int,2> s = {{2, 2}};
+  try {
 
-  typedef Dune::YaspGrid<2> HostGrid;
-  HostGrid hostgrid(L,s);
+    Dune::MPIHelper::instance(argc,argv);
 
-  //typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::FewSubDomainsTraits<2,8> > MDGrid;
-  typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::ArrayBasedTraits<2,8,8> > MDGrid;
-  MDGrid mdgrid(hostgrid,true);
+    Dune::FieldVector<double,2> L(1.0);
+    Dune::array<int,2> s = {{2, 2}};
 
-  typedef MDGrid::LeafGridView MDGV;
-  typedef MDGV::Codim<0>::Iterator Iterator;
+    typedef Dune::YaspGrid<2> HostGrid;
+    HostGrid hostgrid(L,s);
 
-  MDGV mdgv = mdgrid.leafGridView();
+    //typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::FewSubDomainsTraits<2,8> > MDGrid;
+    typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::ArrayBasedTraits<2,8,8> > MDGrid;
+    MDGrid mdgrid(hostgrid,true);
 
-  mdgrid.startSubDomainMarking();
+    typedef MDGrid::LeafGridView MDGV;
+    typedef MDGV::Codim<0>::Iterator Iterator;
 
-  for(Iterator it = mdgv.begin<0>(); it != mdgv.end<0>(); ++it)
+    MDGV mdgv = mdgrid.leafGridView();
+
+    mdgrid.startSubDomainMarking();
+
+    for(Iterator it = mdgv.begin<0>(); it != mdgv.end<0>(); ++it)
       mdgrid.addToSubDomain(0,*it);
 
-  mdgrid.preUpdateSubDomains();
-  mdgrid.updateSubDomains();
-  mdgrid.postUpdateSubDomains();
+    mdgrid.preUpdateSubDomains();
+    mdgrid.updateSubDomains();
+    mdgrid.postUpdateSubDomains();
 
-  typedef MDGrid::SubDomainGrid SDGrid;
-  const SDGrid& sdgrid = mdgrid.subDomain(0);
-  SDGrid::LeafGridView sdgv = sdgrid.leafGridView();
+    typedef MDGrid::SubDomainGrid SDGrid;
+    const SDGrid& sdgrid = mdgrid.subDomain(0);
+    SDGrid::LeafGridView sdgv = sdgrid.leafGridView();
 
-  SDGrid::LeafGridView::Codim<0>::Iterator it = sdgv.begin<0>();
-  SDGrid::LeafGridView::IntersectionIterator iit = sdgv.ibegin(*it);
+    SDGrid::LeafGridView::Codim<0>::Iterator it = sdgv.begin<0>();
+    SDGrid::LeafGridView::IntersectionIterator iit = sdgv.ibegin(*it);
 
-  const MDGrid::LeafGridView::Intersection& is1 = sdgrid.multiDomainIntersection(*iit);
-  const MDGrid::LeafGridView::Intersection& is2 = mdgrid.multiDomainIntersection(*iit);
+    const MDGrid::LeafGridView::Intersection& is1 DUNE_UNUSED = sdgrid.multiDomainIntersection(*iit);
+    const MDGrid::LeafGridView::Intersection& is2 DUNE_UNUSED = mdgrid.multiDomainIntersection(*iit);
 
-  assert(is1.geometry().center() == is2.geometry().center());
+    assert(is1.geometry().center() == is2.geometry().center());
+
+    return 0;
+
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+  }
+
 }
