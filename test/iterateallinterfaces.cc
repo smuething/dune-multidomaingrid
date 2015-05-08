cda7f57ec74fbc131f8c1bcb61d685cff8df39ea
Steffen Müthing <muething@dune-project.org>
2015-05-08 17:46:00 +0200

    [Tests] Clean up iterateallinterfaces
    - Use new entity-based grid API
    
    - Use range-based for loops
    
    - Make the test callable without command line arguments by providing
      default values
    
    - Initialize MPI to cope with parallel-by-default YaspGrid
    
    - Catch exceptions

--- test/iterateallinterfaces.cc
+++ test/iterateallinterfaces.cc
@@ -13,24 +13,22 @@ void assembleLocalInterfaceTerm(ST s1, const E& e1, ST s2, const E& e2)
 template<typename MDGV>
 void iterate(MDGV mdgv)
 {
-  typedef typename MDGV::template Codim<0>::Iterator Iterator;
-  typedef typename MDGV::template Codim<0>::EntityPointer EP;
-  typedef typename MDGV::IntersectionIterator IIterator;
+  typedef typename MDGV::template Codim<0>::Entity Entity;
   typedef typename MDGV::IndexSet::SubDomainIndex SubDomainIndex;
   const typename MDGV::IndexSet& is = mdgv.indexSet();
 
-  for (Iterator it = mdgv.template begin<0>(); it != mdgv.template end<0>(); ++it)
+  for (const auto& cell : elements(mdgv))
     {
       // this assumes that subdomains form a partition of the host grid
-      const SubDomainIndex insideSubDomain = *is.subDomains(*it).begin();
-      for (IIterator iit = mdgv.ibegin(*it); iit != mdgv.iend(*it); ++iit)
+      const SubDomainIndex insideSubDomain = *is.subDomains(cell).begin();
+      for (const auto& intersection : intersections(mdgv,cell))
         {
-          if (!iit->neighbor())
+          if (!intersection.neighbor())
             continue;
-          const EP outside = iit->outside();
-          const SubDomainIndex outsideSubDomain = *is.subDomains(*outside).begin();
+          const Entity outside = intersection.outside();
+          const SubDomainIndex outsideSubDomain = intersection.subDomains(outside).begin();
           if (insideSubDomain != outsideSubDomain)
-            assembleLocalInterfaceTerm(insideSubDomain,*it,outsideSubDomain,*outside);
+            assembleLocalInterfaceTerm(insideSubDomain,cell,outsideSubDomain,outside);
         }
     }
 }
@@ -39,19 +37,19 @@ template<typename Grid>
 void iterate2(const Grid& grid)
 {
   typedef typename Grid::LeafAllSubDomainInterfacesIterator Iterator;
-  typedef typename Grid::template Codim<0>::EntityPointer EP;
+  typedef typename Grid::template Codim<0>::Entity Entity;
 
   for (Iterator it = grid.leafAllSubDomainInterfacesBegin(); it != grid.leafAllSubDomainInterfacesEnd(); ++it)
     {
-      const EP ep1 = it->firstCell();
-      const EP ep2 = it->secondCell();
-      std::cout << it->subDomain1() << " -> " << it->subDomain2() << " (" << ep1->geometry().center() << " -> " << ep2->geometry().center() << ") " << it->geometry().center() << std::endl;
+      const Entity e1 = it->firstCell();
+      const Entity e2 = it->secondCell();
+      std::cout << it->subDomain1() << " -> " << it->subDomain2() << " (" << e1.geometry().center() << " -> " << e2.geometry().center() << ") " << it->geometry().center() << std::endl;
     }
   for (auto it = grid.leafSubDomainInterfaceBegin(0,1); it != grid.leafSubDomainInterfaceEnd(0,1); ++it)
     {
-      const EP ep1 = it->firstCell();
-      const EP ep2 = it->secondCell();
-      std::cout << it->subDomain1() << " -> " << it->subDomain2() << " (" << ep1->geometry().center() << " -> " << ep2->geometry().center() << ") " << it->geometry().center() << std::endl;
+      const Entity e1 = it->firstCell();
+      const Entity e2 = it->secondCell();
+      std::cout << it->subDomain1() << " -> " << it->subDomain2() << " (" << e1.geometry().center() << " -> " << e2.geometry().center() << ") " << it->geometry().center() << std::endl;
     }
 }
 
@@ -60,29 +58,28 @@ template<typename MDGrid>
 void driver(MDGrid& mdgrid)
 {
   typedef typename MDGrid::LeafGridView MDGV;
-  typedef typename MDGV::template Codim<0>::Iterator Iterator;
   typedef typename MDGrid::SubDomainIndex SubDomainIndex;
 
   MDGV mdgv = mdgrid.leafGridView();
 
   mdgrid.startSubDomainMarking();
 
-  for(Iterator it = mdgv.template begin<0>(); it != mdgv.template end<0>(); ++it)
+  for(const auto& cell : elements(mdgv))
     {
       SubDomainIndex subdomain = 0;
-      if (it->geometry().center()[0] > 0.5)
+      if (cell.geometry().center()[0] > 0.5)
         subdomain += 1;
-      if (it->geometry().center()[1] > 0.5)
+      if (cell.geometry().center()[1] > 0.5)
         subdomain += 2;
-      mdgrid.addToSubDomain(subdomain,*it);
-      if (it->geometry().center()[1] > 0.5)
-        mdgrid.addToSubDomain(4,*it);
-      if (it->geometry().center()[0] > 0.5)
-        mdgrid.addToSubDomain(5,*it);
-      if (it->geometry().center()[0] > 0.5 && it->geometry().center()[1] > 0.5)
-        mdgrid.addToSubDomain(6,*it);
-      if (it->geometry().center()[0] > 0.5 && it->geometry().center()[1] < 0.5)
-        mdgrid.addToSubDomain(7,*it);
+      mdgrid.addToSubDomain(subdomain,cell);
+      if (cell.geometry().center()[1] > 0.5)
+        mdgrid.addToSubDomain(4,cell);
+      if (cell.geometry().center()[0] > 0.5)
+        mdgrid.addToSubDomain(5,cell);
+      if (cell.geometry().center()[0] > 0.5 && cell.geometry().center()[1] > 0.5)
+        mdgrid.addToSubDomain(6,cell);
+      if (cell.geometry().center()[0] > 0.5 && cell.geometry().center()[1] < 0.5)
+        mdgrid.addToSubDomain(7,cell);
     }
 
   mdgrid.preUpdateSubDomains();
@@ -96,37 +93,55 @@ void driver(MDGrid& mdgrid)
 
 int main(int argc, char** argv)
 {
-  if (argc < 2)
+
+  try {
+
+    Dune::MPIHelper::instance(argc,argv);
+
+    int refinement = 5;
+
+    if (argc < 2)
+      {
+        std::cerr << "Usage: " << argv[0] << " <refinement level>" << std::endl;
+        std::cerr << std::endl << "Defaulting to refinement level = " << refinement << std::endl;
+      }
+    else
+      refinement = atoi(argv[1]);
+
+    Dune::FieldVector<double,2> L(1.0);
+    Dune::array<int,2> s = {{2, 2}};
+
+    typedef Dune::YaspGrid<2> HostGrid;
+    HostGrid hostgrid(L,s);
+    hostgrid.globalRefine(refinement);
+
     {
-      std::cerr << "Usage: " << argv[0] << " <refinement level>" << std::endl;
-      exit(1);
+      typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::FewSubDomainsTraits<2,8> > MDGrid;
+      MDGrid mdgrid(hostgrid,true);
+      driver(mdgrid);
     }
 
-  Dune::FieldVector<double,2> L(1.0);
-  Dune::array<int,2> s = {{2, 2}};
+    {
+      typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::ArrayBasedTraits<2,8,8> > MDGrid;
+      MDGrid mdgrid(hostgrid,true);
+      driver(mdgrid);
+    }
 
-  typedef Dune::YaspGrid<2> HostGrid;
-  HostGrid hostgrid(L,s);
-  hostgrid.globalRefine(atoi(argv[1]));
+    {
+      typedef Dune::mdgrid::DynamicSubDomainCountTraits<2,8> Traits;
+      typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::DynamicSubDomainCountTraits<2,8> > MDGrid;
+      MDGrid mdgrid(hostgrid,Traits(8),true);
+      driver(mdgrid);
+    }
 
-  {
-    typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::FewSubDomainsTraits<2,8> > MDGrid;
-    MDGrid mdgrid(hostgrid,true);
-    driver(mdgrid);
+    return 0;
+
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
   }
 
-  {
-    typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::ArrayBasedTraits<2,8,8> > MDGrid;
-    MDGrid mdgrid(hostgrid,true);
-    driver(mdgrid);
-  }
-
-  {
-    typedef Dune::mdgrid::DynamicSubDomainCountTraits<2,8> Traits;
-    typedef Dune::MultiDomainGrid<HostGrid,Dune::mdgrid::DynamicSubDomainCountTraits<2,8> > MDGrid;
-    MDGrid mdgrid(hostgrid,Traits(8),true);
-    driver(mdgrid);
-  }
-
-  return 0;
 }
